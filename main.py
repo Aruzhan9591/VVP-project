d50df8a5bced265ae082234586567751246f3891
Aruzhan9591 <128011163+Aruzhan9591@users.noreply.github.com>
2025-05-17 16:55:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from maze_lib.loader import load_maze
 from maze_lib.bfs_solver import bfs
 from maze_lib.visualizer import generate_image
+from maze_lib.generator import generate_maze
 
 for i in range(1, 6):
     file_path = f"maze_{i}.csv"
@@ -13,6 +14,6 @@ for i in range(1, 6):
 
 # new generated maze
 generated_maze = generate_maze(size=20, density=0.3)
-generated_path = Bfs(generated_maze)
+generated_path = bfs(generated_maze)
 generate_image(generated_maze, generated_path, "generated_maze.png")
 print("[OK] Generated random maze → generated_maze.png")
